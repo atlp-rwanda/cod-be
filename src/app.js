ee0fd195ae9ab3ca1bc67e1e1eca29ea02f068e0
iyaremye-faustin <iyaremyef@gmail.com>
2022-03-03 21:18:25 +0200

    chore setting boilerplate-changes 181414542

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv  from 'dotenv';
 const app=express();
 dotenv.config({path:__dirname+'/../.env'});
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`Server running on port: ${port}`);
 });
