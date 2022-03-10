e6dfd5d26254dcc0b7d6002e73761da8bfdde679
Alex Axel <mucyoalexaxel@gmail.com>
2022-03-10 12:01:16 +0200

    Fix ES-Lint Errors

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,8 +1,16 @@
+/* eslint-disable global-require */
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-path-concat */
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable prefer-template */
+/* eslint-disable strict */
+
 'use strict';
 
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
@@ -17,15 +25,15 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
+  .filter((file) => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(file => {
+  .forEach((file) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
