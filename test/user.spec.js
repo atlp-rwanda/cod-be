94157ec7ef718d64fdae41c505a8f9808a79b0eb
iyaremye-faustin <iyaremyef@gmail.com>
2022-03-15 23:11:53 +0200

    Fix rebase

--- test/user.spec.js
+++ test/user.spec.js
@@ -12,31 +12,32 @@ const user={
     lastname:'IYAREMYE',
     email:'testemail@me.com',
     password:'test1234@5678'}
-// describe('/POST  endpoint', () => {
-//   it('it should register a new user', (done) => {
-//     chai.request(server)
-//         .post('/api/user/register')
-//         .set('content-type', 'application/json')
-//         .send(user)
-//         .end((err, res) => {
-//               res.should.have.status(201);
-//           done();
-//         });
-//   });
-// })
-// describe('/POST  endpoint', () => {
-//   after(async()=>{
-//     await Users.destroy({where: { email: `${user.email}` }});
-//   });
-//     it('it should return email already exists', (done) => {
-//       chai.request(server)
-//           .post('/api/user/register')
-//           .set('content-type', 'application/json')
-//           .send(user)
-//           .end((err, res) => {
-//                 res.should.have.status(409);
-//                 res.body.should.have.property('Error');
-//             done();
-//           });
-//     });
-//   })
+describe('/POST  endpoint', () => {
+  it('it should register a new user', (done) => {
+    chai.request(server)
+        .post('/api/user/register')
+        .set('content-type', 'application/json')
+        .send(user)
+        .end((err, res) => {
+              res.should.have.status(201);
+              res.body.should.have.property('accessToken');
+          done();
+        });
+  });
+})
+describe('/POST  endpoint', () => {
+  after(async()=>{
+    await Users.destroy({where: { email: `${user.email}` }});
+  });
+    it('it should return email already exists', (done) => {
+      chai.request(server)
+          .post('/api/user/register')
+          .set('content-type', 'application/json')
+          .send(user)
+          .end((err, res) => {
+                res.should.have.status(409);
+                res.body.should.have.property('Error');
+            done();
+          });
+    });
+  })
