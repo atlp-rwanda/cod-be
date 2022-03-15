b51139948fb07a20975247b2257730e64a4799db
iyaremye-faustin <iyaremyef@gmail.com>
2022-03-15 21:58:22 +0200

    reabsing

---
+++ src/database/models/role.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Role.hasMany(models.User, {
+        foreignKey: 'roleId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      })
+    }
+  }
+  Role.init({
+    roleName: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Role',
+    timestamps: true,
+  });
+  return Role;
+};
---
+++ src/services/userService.js
@@ -0,0 +1,11 @@
+import models from '../database/models';
+const { User } = models;
+const addUser=async (newUser)=>{
+  const user=await  User.create(newUser);
+  return user;
+}
+const findByEmail=async(email)=>{
+  const user = await User.findOne({ where: { email: `${email}` } });
+  return user;
+}
+export {addUser,findByEmail};
