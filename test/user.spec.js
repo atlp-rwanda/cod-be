0a89d9d0d54ecf404817052e8e6e7f987cddacbb
iyaremye-faustin <iyaremyef@gmail.com>
2022-03-15 23:42:19 +0200

    Fix: tests

--- test/user.spec.js
+++ test/user.spec.js
@@ -14,35 +14,30 @@ const user={
     email:'testemail@me.com',
     password:'test1234@5678'}
 describe('/POST  endpoint', () => {
-  it('Should save a new user', async () => {
-    const res = await request(server).post('/api/user/register').send(user);
-    expect(res).to.have.status(201);
+  it('it should register a new user', (done) => {
+    chai.request(server)
+        .post('/api/user/register')
+        .set('content-type', 'application/json')
+        .send(user)
+        .end((err, res) => {
+              res.should.have.status(201);
+          done();
+        });
   });
-  // it('it should register a new user', (done) => {
-  //   chai.request(server)
-  //       .post('/api/user/register')
-  //       .set('content-type', 'application/json')
-  //       .send(user)
-  //       .end((err, res) => {
-  //             res.should.have.status(201);
-  //             res.body.should.have.property('accessToken');
-  //         done();
-  //       });
-  // });
 })
-// describe('/POST  endpoint', () => {
-//   after(async()=>{
-//     await Users.destroy({where: { email: `${user.email}` }});
-//   });
-//     it('it should return email already exists', (done) => {
-//       chai.request(server)
-//           .post('/api/user/register')
-//           .set('content-type', 'application/json')
-//           .send(user)
-//           .end((err, res) => {
-//                 res.should.have.status(409);
-//                 res.body.should.have.property('Error');
-//             done();
-//           });
-//     });
-//   })
+describe('/POST  endpoint', () => {
+  after(async()=>{
+    await Users.destroy({where: { email: `${user.email}` }});
+  });
+    it('it should return email already exists', (done) => {
+      chai.request(server)
+          .post('/api/user/register')
+          .set('content-type', 'application/json')
+          .send(user)
+          .end((err, res) => {
+                res.should.have.status(409);
+                res.body.should.have.property('Error');
+            done();
+          });
+    });
+  })
