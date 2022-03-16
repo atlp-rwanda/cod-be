ca656310655065cde3b8a51e8f8e10e749e22427
iyaremye-faustin <iyaremyef@gmail.com>
2022-03-16 06:05:30 +0200

    Fix:test:

--- test/user.spec.js
+++ test/user.spec.js
@@ -14,12 +14,18 @@ const user={
     email:'testemail@me.com',
     password:'test1234@5678'}
 describe('/POST  register endpoint', () => {
+  after(async()=>{
+    await Users.destroy({where: { email: `${user.email}` }});
+  });
   it('it should register a new user', async () => {
     const res = await request(server).post('/api/user/register').send(user);
     expect(res).to.have.status(201);
     expect(res.body).to.have.property('accessToken');
   });
-
+  it('it should return email already exists', async () => {
+    const res = await request(server).post('/api/user/register').send(user);
+    expect(res).to.have.status(409);
+  });
   // it('it should register a new user', (done) => {
   //   chai.request(server)
   //       .post('/api/user/register')
@@ -31,23 +37,19 @@ describe('/POST  register endpoint', () => {
   //       });
   // });
 })
-describe('/POST  endpoint', () => {
-  after(async()=>{
-    await Users.destroy({where: { email: `${user.email}` }});
-  });
-    // it('it should return email already exists', (done) => {
-    //   chai.request(server)
-    //       .post('/api/user/register')
-    //       .set('content-type', 'application/json')
-    //       .send(user)
-    //       .end((err, res) => {
-    //             res.should.have.status(409);
-    //             res.body.should.have.property('Error');
-    //         done();
-    //       });
-    // });
-    it('it should return email already exists', async () => {
-      const res = await request(server).post('/api/user/register').send(user);
-      expect(res).to.have.status(409);
-    });
-  })
+// describe('/POST  endpoint', () => {
+//   after(async()=>{
+//     await Users.destroy({where: { email: `${user.email}` }});
+//   });
+//     // it('it should return email already exists', (done) => {
+//     //   chai.request(server)
+//     //       .post('/api/user/register')
+//     //       .set('content-type', 'application/json')
+//     //       .send(user)
+//     //       .end((err, res) => {
+//     //             res.should.have.status(409);
+//     //             res.body.should.have.property('Error');
+//     //         done();
+//     //       });
+//     // });
+//   })
