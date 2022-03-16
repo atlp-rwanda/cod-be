3f078391769473ac8b8a601c20102ed07e5e05f7
iyaremye-faustin <iyaremyef@gmail.com>
2022-03-16 05:49:46 +0200

    Fix:test

--- test/user.spec.js
+++ test/user.spec.js
@@ -1,5 +1,5 @@
 import chai from "chai";
-import { expect, request, use } from 'chai';
+import { expect, request, use,} from 'chai';
 import chaiHttp from "chai-http";
 import server from "../src/app.js";   
 import models from '../src/database/models';
@@ -13,31 +13,41 @@ const user={
     lastname:'IYAREMYE',
     email:'testemail@me.com',
     password:'test1234@5678'}
-describe('/POST  endpoint', () => {
-  it('it should register a new user', (done) => {
-    chai.request(server)
-        .post('/api/user/register')
-        .set('content-type', 'application/json')
-        .send(user)
-        .end((err, res) => {
-              res.should.have.status(201);
-          done();
-        });
+describe('/POST  register endpoint', () => {
+  it('it should register a new user', async () => {
+    const res = await request(server).post('/api/user/register').send(user);
+    expect(res).to.have.status(201);
+    expect(res.type).to.equal('application/json');
   });
+
+  // it('it should register a new user', (done) => {
+  //   chai.request(server)
+  //       .post('/api/user/register')
+  //       .set('content-type', 'application/json')
+  //       .send(user)
+  //       .end((err, res) => {
+  //             res.should.have.status(201);
+  //         done();
+  //       });
+  // });
 })
 describe('/POST  endpoint', () => {
   after(async()=>{
     await Users.destroy({where: { email: `${user.email}` }});
   });
-    it('it should return email already exists', (done) => {
-      chai.request(server)
-          .post('/api/user/register')
-          .set('content-type', 'application/json')
-          .send(user)
-          .end((err, res) => {
-                res.should.have.status(409);
-                res.body.should.have.property('Error');
-            done();
-          });
+    // it('it should return email already exists', (done) => {
+    //   chai.request(server)
+    //       .post('/api/user/register')
+    //       .set('content-type', 'application/json')
+    //       .send(user)
+    //       .end((err, res) => {
+    //             res.should.have.status(409);
+    //             res.body.should.have.property('Error');
+    //         done();
+    //       });
+    // });
+    it('it should return email already exists', async () => {
+      const res = await request(server).post('/api/user/register').send(user);
+      expect(res).to.have.status(409);
     });
   })
